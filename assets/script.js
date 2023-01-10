365eb026d92a3b55dad349dccb8ca1e692737e52
amsudekum <116687202+amsudekum@users.noreply.github.com>
2023-01-09 22:56:32 -0600

    wrote arrays for questions and answers

---
+++ assets/script.js
@@ -0,0 +1,74 @@
+let questions = [
+    {
+        "text": 'What is a const?',
+        "options": [
+            'A. A variable.', 'B. A variable that cannot be changed.', 'C. It is constant.', 'D. It is a constable.'
+        ]
+    },
+
+    {
+        "text": 'Which of these data types is a string?',
+        "options": [
+            'A. A string.', " B. 'A string.'", 'C. String.', 'D. More string.'
+        ]
+    },
+
+    {
+        "text": 'Which operator checks that the value and data types are the same?',
+        "options": [
+            'A. =', 'B. ==', 'C. !==' , 'D. ==='
+        ]
+    },
+
+    {
+        "text": 'How do you call a function?',
+        "options": [
+            'A. callFunction', 'B. callfunction()', 'C. callFunction()', 'D. CALLFUNCTION!'
+        ]
+    },
+
+    {
+        "text": 'What is a parameter?',
+        "options": [
+            'A. A para-meter', 'B. A meter.', 'C. Allows functions to accept inputs.', 'D. Paramaters do not exist.'
+        ]
+    },
+
+    {
+        "text": '>What is the numbered position in an array?',
+        "options": [
+            'A. 0', 'B. 0.0', 'C. 1', 'D. 1.0'
+        ]
+    },
+
+    {
+        "text": 'What does .length do?',
+        "options": [
+            'A. Gives the length of your string in px.', 'B. Gives the length of your string in inches.', 'C. Gives the length of all your code.', 'D. Returns the length of an object.'
+        ]
+    },
+
+    {
+        "text": 'What does a loop do?',
+        "options": [
+            'A. Repeats a set of instructions until a specified condition is met. ', 'B. It loops your code around.', 'C. It resets your code.', 'D. It transform your code into a loop.'
+        ]
+    },
+
+    {
+        "text": 'What does .forEach() do?',
+        "options": [
+            'A. Executes the same code for each element.', 'B. Lists elements of an array. ', 'C. Lists every array in a project.', 'D. It does each and everything it can.'
+        ]
+    },
+
+    {
+        "text": 'What does an Else If statement do?',
+        "options": [
+            'A. Complicates our code.', 'B. Gives other developers something fun to read during review.', 'C. Allows for more than two possible incomes in an If statement.', 'D. Makes If statements harder to read. '
+        ]
+    }
+];
+
+
+
